582ea3a835e27e1be80ecf1449559dbde74f6271
Abhishek <abhikannor12@gmail.com>
2024-01-23 13:49:00 +0530

    adding sorting topic

---
+++ sorting.py
@@ -0,0 +1,13 @@
+# BUBBLE SORT 
+
+
+def sortB(arr):
+    i = 0
+    for j in range(len(arr)-1):
+        if arr[j] < arr[j+1]:
+            pass
+        else:
+            return False
+    return True
+print(sortB([1,2,3,4]))
+
