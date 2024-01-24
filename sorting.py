96f8f27210be73a4d5127205b8c1752b373b6dc7
Abhishek <abhikannor12@gmail.com>
2024-01-24 15:11:09 +0530

    modification

--- sorting.py
+++ sorting.py
@@ -1,128 +1,128 @@
-# array is Sorted or not 
+array is Sorted or not 
 
-# def sortB(arr):
-#     i = 0
-#     for j in range(len(arr)-1):
-#         if arr[j] < arr[j+1]:
-#             pass
-#         else:
-#             return False
-#     return True
-# # print(sortB([1,2,3,4])) 
-
-
-# Bubble sorting -- asending order o(n*n)-- worst case best case O(n)
-# def bubble(arr):
-#     for i in range(len(arr)-1):
-#         sorting = False
-#         for j in range(1,len(arr)-i):
-#             if arr[j] < arr[j-1]:
-#                 arr[j],arr[j-1] = arr[j-1],arr[j]
-#                 sorting =  True
-#         if sorting == False:
-#             break
-#     return arr
-# print(bubble([5,8,3,65,-43,0,12]))
-
-# # Bubble sorting -- Dsending order o(n*n)-- worst case best case O(n)
-# def bubble(arr):
-#     for i in range(len(arr)-1):
-#         sorting = False
-#         for j in range(1,len(arr)):
-#             if arr[j] > arr[j-1]:
-#                 arr[j],arr[j-1] = arr[j-1],arr[j]
-#                 sorting =  True
-#         if sorting == False:
-#             break
-#     return arr
-# print(bubble([5,8,3,65,-43,0,12]))
+def sortB(arr):
+    i = 0
+    for j in range(len(arr)-1):
+        if arr[j] < arr[j+1]:
+            pass
+        else:
+            return False
+    return True
+# print(sortB([1,2,3,4])) 
 
 
-# # Selection sort  best / worst / average complexity ---O(n*n) taking minimum element and put into first index
-# def selection_sort(arr):
-#     for i in range(len(arr)):
-#         mini = i
-#         for j in range(i,len(arr)):
-#             if arr[j] < arr[mini]:
-#                 mini = j
-#         temp = arr[mini]
-#         arr[mini] = arr[i]
-#         arr[i] = temp
-#     return arr
-# print(selection_sort( [-23,0,3,-33]))
+Bubble sorting -- asending order o(n*n)-- worst case best case O(n)
+def bubble(arr):
+    for i in range(len(arr)-1):
+        sorting = False
+        for j in range(1,len(arr)-i):
+            if arr[j] < arr[j-1]:
+                arr[j],arr[j-1] = arr[j-1],arr[j]
+                sorting =  True
+        if sorting == False:
+            break
+    return arr
+print(bubble([5,8,3,65,-43,0,12]))
 
-# Selection sort  best / worst / average complexity ---O(n*n) taking max element and put into last index
-# def selection_sort(arr):
-#     for i in range(len(arr)):
-#         max = len(arr)-1-i
-#         for j in range(i,len(arr)):
-#             if arr[j] > arr[max]:
-#                 max = j
-#         temp = arr[max]
-#         arr[max] = arr[i]
-#         arr[i] = temp
-#     return arr
-# print(selection_sort( [-23,0,3,-33]))
-
-#  insertion sort  best complexity O(n) worst avg -- O(n*n)
-
-# def insertion_sort(arr):
-#     for i in range(len(arr)):
-#         j = i
-#         while(j > 0 and arr[j-1] > arr[j]):
-#             arr[j],arr[j-1] = arr[j-1],arr[j]
-#             j -= 1
-#     return arr
-# print(insertion_sort([-23,0,3,-33]))
-
-# merge sort 
-# def merge(l,h):
-#     if l == h:
-#         return 
-#     if (l < h):
-#         mid = (l+h)/2
-#         merge(l,mid)
-#         merge(mid+1,h)
-#         merge(l,mid,h)
-#     return 
-# arr =[4,2,5,7,8]
-# print(merge(0,len(arr)))
+# Bubble sorting -- Dsending order o(n*n)-- worst case best case O(n)
+def bubble(arr):
+    for i in range(len(arr)-1):
+        sorting = False
+        for j in range(1,len(arr)):
+            if arr[j] > arr[j-1]:
+                arr[j],arr[j-1] = arr[j-1],arr[j]
+                sorting =  True
+        if sorting == False:
+            break
+    return arr
+print(bubble([5,8,3,65,-43,0,12]))
 
 
-# merge sort
-# def merged(arr):
-#     if len(arr) > 1:
-#         left_arr = arr[:len(arr)//2]
-#         right_arr = arr[len(arr)//2:]
+# Selection sort  best / worst / average complexity ---O(n*n) taking minimum element and put into first index
+def selection_sort(arr):
+    for i in range(len(arr)):
+        mini = i
+        for j in range(i,len(arr)):
+            if arr[j] < arr[mini]:
+                mini = j
+        temp = arr[mini]
+        arr[mini] = arr[i]
+        arr[i] = temp
+    return arr
+print(selection_sort( [-23,0,3,-33]))
 
-#         merged(left_arr)
-#         merged(right_arr)
-#         #  merge
-#         i = 0
-#         j = 0
-#         k = 0
+Selection sort  best / worst / average complexity ---O(n*n) taking max element and put into last index
+def selection_sort(arr):
+    for i in range(len(arr)):
+        max = len(arr)-1-i
+        for j in range(i,len(arr)):
+            if arr[j] > arr[max]:
+                max = j
+        temp = arr[max]
+        arr[max] = arr[i]
+        arr[i] = temp
+    return arr
+print(selection_sort( [-23,0,3,-33]))
 
-#         while i < len(left_arr) and j < len(right_arr):
-#             if left_arr[i] < right_arr[j]:
-#                 arr[k] = left_arr[i]
-#                 i += 1
-#                 k += 1
-#             else:
-#                 arr[k] = right_arr[j]
-#                 j += 1
-#                 k += 1
-#         while (i < len(left_arr)):
-#             arr[k] = left_arr[i]
-#             i += 1
-#             k += 1
-#         while( j < len(right_arr)):
-#             arr[k] = right_arr[j]
-#             j += 1
-#             k += 1
+ insertion sort  best complexity O(n) worst avg -- O(n*n)
 
-# arr = [-23,0,4,2,1,-33]
-# merged(arr)
-# print(arr)
+def insertion_sort(arr):
+    for i in range(len(arr)):
+        j = i
+        while(j > 0 and arr[j-1] > arr[j]):
+            arr[j],arr[j-1] = arr[j-1],arr[j]
+            j -= 1
+    return arr
+print(insertion_sort([-23,0,3,-33]))
+
+merge sort 
+def merge(l,h):
+    if l == h:
+        return 
+    if (l < h):
+        mid = (l+h)/2
+        merge(l,mid)
+        merge(mid+1,h)
+        merge(l,mid,h)
+    return 
+arr =[4,2,5,7,8]
+print(merge(0,len(arr)))
+
+
+merge sort
+def merged(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        merged(left_arr)
+        merged(right_arr)
+        #  merge
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+                k += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+                k += 1
+        while (i < len(left_arr)):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        while( j < len(right_arr)):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+arr = [-23,0,4,2,1,-33]
+merged(arr)
+print(arr)
 
 #  quick sort 
 def quickSort(arr,low,high):
